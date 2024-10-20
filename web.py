78385c28bc39613558cc8ac772d3b353b5d27d49
OmerDeger6 <skype.com37@gmail.com>
2024-10-20 20:09:51 +0300

    Create web.py

---
+++ web.py
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Başlık</title>
+    <link rel="stylesheet" href="style.css">
+
+
+
+
+</head>
+<body>
+<h1>Teknoloji Bağımlılığı  </h1>
+<p>Bu, bir kişinin belirli teknolojiler ve cihazlar olmadan yapamadığı bir bağımlılık türüdür.
+    Bu teknolojilere olan aşırı bağımlılık kişinin fonksiyonel yeteneklerini önemli ölçüde köreltebilir. Bu da yaşam kalitesi üzerinde ciddi olumsuz etkilere neden olabilir.</p>    
+<h2>Teknoloji Bağımlılığı Ile Bas Etmenin Yolları</h2>
+
+
+<ul>
+    <li>Sınırlı zaman </li>
+    <li>Dışarıda vakit geçirme  </li>
+    <li>Daha fazla inanlarla konuşmak </li>
+</ul>
+
+<img src="https://media3.giphy.com/media/26BGGxgsEh42ABHFe/giphy.gif?cid=ecf05e47fewnmm2h6vijgpd1768vz8zxo441mpec89684gtw&rid=giphy.gif&ct=g" alt="Image">
+
+<iframe src="https://www.meteoblue.com/tr/hava/widget/daily/ip_romanya_675514?geoloc=fixed&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=1&maxtemperature=1&mintemperature=1&windspeed=1&windgust=0&winddirection=1&uv=0&humidity=0&precipitation=1&precipitationprobability=1&spot=1&pressure=0&layout=light"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 216px; height: 420px"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/tr/hava/hafta/ip_romanya_675514?utm_source=daily_widget&utm_medium=linkus&utm_content=daily&utm_campaign=Weather%2BWidget" target="_blank" rel="noopener">meteoblue</a></div>
+
+<iframe src="https://www.meteoblue.com/tr/hava/widget/three/ip_romanya_675514?geoloc=fixed&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 460px; height: 594px"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/tr/hava/hafta/ip_romanya_675514?utm_source=three_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget" target="_blank" rel="noopener">meteoblue</a></div>
+
+
+</body>
+</html>
